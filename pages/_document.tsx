190d475efc7dd71e31da3b6024262e2c19825720
Enzo <enzovivallo05@outlook.com>
2022-02-18 18:04:41 -0300

    fix: add type to context

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,14 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document'
 import { CssBaseline } from '@nextui-org/react'
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx)
     return {
       ...initialProps,
